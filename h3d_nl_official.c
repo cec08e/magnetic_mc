5faf6ac2cc77b5c9a12126143485dee2292d2a77
Caitlin Carnahan <caitlin@Caitlins-MacBook-Pro.local>
2018-12-14 15:59:38 -0500

    Adding files.

---
+++ h3d_nl_official.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_sf_exp.h>
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
+#include <gsl/gsl_sf_pow_int.h>
+#include <time.h>
+#include "h3d_nl.h"
+
+
+/*
+Requirements: GNU Scientific Library (gsl) and CBLAS
+
+gcc -lgsl -lgslcblas h3d_nl.c
+ALT: gcc -fPIC -shared -o h3d_nl.so -lgsl -lgslcblas h3d_nl.c
+
+************ N LAYER VERSION -- OFFICIAL, UPDATED *****************
+*/
+
+
+int main(){
+  parse_config_file();
+  echo_params(stdout);
+  build_lattice();
+}
+
+void parse_config_file(){
+  /* to do: make structure more permissable - whitespace allowance, etc. */
+  /* MUST ALLOCATE SPACE TO PARAM LISTS BEFORE ACCEPTING VALUES */
+
+  FILE* fp;
+  char config_line[80];
+  char param[80];
+  char val_list[80], *str_val;
+  int val, i;
+  float val_d;
+
+  fp = fopen(CONFIG_FILE, "r");
+  while(fgets(config_line, 80,fp)){
+    if(sscanf(config_line, "%s = %d\n", param, &val) == 2){
+      /* Case includes: SIM_NUM, NUM_L, NUM_R, NUM_C, OVER_FLAG, ANNEAL_TIME,
+      EQ_TIME, COR_TIME, J_INTRA_P, J_INTER_P, D_INTRA_P, D_INTER_P, K_P*/
+      if(!strcmp(param, "SIM_NUM")) SIM_NUM = val;
+      else if(!strcmp(param, "NUM_L")){
+         NUM_L = val;
+         /* Number of layers now known. Allocate space for constant
+         interaction strength parameters */
+         J_INTRA_CONST = malloc(NUM_L*sizeof(float));
+         J_INTER_CONST = malloc(NUM_L*sizeof(float));
+         D_INTRA_CONST = malloc(NUM_L*sizeof(float));
+         D_INTER_CONST = malloc(NUM_L*sizeof(float));
+         K_CONST = malloc(NUM_L*sizeof(float));
+
+       }
+      else if(!strcmp(param, "NUM_R")) NUM_R = val;
+      else if(!strcmp(param, "NUM_C")) NUM_C = val;
+      else if(!strcmp(param, "OVER_FLAG")) OVER_FLAG = val;
+      else if(!strcmp(param, "ANNEAL_TIME")) ANNEAL_TIME = val;
+      else if(!strcmp(param, "EQ_TIME")) EQ_TIME = val;
+      else if(!strcmp(param, "COR_TIME")) COR_TIME = val;
+      else if(!strcmp(param, "J_INTRA_P")) J_INTRA_P = val;
+      else if(!strcmp(param, "J_INTER_P")) J_INTER_P = val;
+      else if(!strcmp(param, "D_INTRA_P")) D_INTRA_P = val;
+      else if(!strcmp(param, "D_INTER_P")) J_INTER_P = val;
+      else if(!strcmp(param, "K_P")) K_P = val;
+    }
+    else if(sscanf(config_line, "%s = %f", param, &val_d) == 2){
+      /* Case includes: B_CONST, J_INTRA_CONST, J_INTER_CONST, D_INTRA_CONST, D_INTER_CONST, K_CONST*/
+      if(!strcmp(param, "INIT_T")) INIT_T = val_d;
+      else if(!strcmp(param, "FINAL_T")) FINAL_T = val_d;
+      else if(!strcmp(param, "DELTA_T")) DELTA_T = val_d;
+      else if(!strcmp(param, "B_CONST")) B_CONST = val_d;
+      else if(!strcmp(param, "DELTA_B")) DELTA_B = val_d;
+      else if(!strcmp(param, "RADIUS")) RADIUS = val_d;
+
+    }
+    else if(sscanf(config_line, "%s = [%s]\n", param, val_list) == 2){
+      i=0;
+      str_val = strtok(val_list,",");
+      if(!strcmp(param, "J_INTRA_CONST")){
+        while (str_val!= NULL){
+          J_INTRA_CONST[i++] = atof(str_val);
+          str_val = strtok (NULL, ",");
+        }
+      }
+      else if(!strcmp(param, "J_INTER_CONST")){
+        while (str_val != NULL){
+          J_INTER_CONST[i++] = atof(str_val);
+          str_val = strtok (NULL, ",");
+        }
+      }
+      else if(!strcmp(param, "D_INTRA_CONST")){
+        while (str_val != NULL){
+          D_INTRA_CONST[i++] = atof(str_val);
+          str_val = strtok (NULL, ",");
+        }
+      }
+      else if(!strcmp(param, "D_INTER_CONST")){
+        while (str_val != NULL){
+          D_INTER_CONST[i++] = atof(str_val);
+          str_val = strtok (NULL, ",");
+        }
+      }
+      else if(!strcmp(param, "K_CONST")){
+        while (str_val != NULL){
+          K_CONST[i++] = atof(str_val);
+          str_val = strtok (NULL, ",");
+        }
+      }
+    }
+    else{
+      printf("Invalid line encountered in configuration file: %s\n", config_line);
+    }
+
+  }
+}
+
+void echo_params(FILE *fp){
+  int i = 0;
+  /* Print the parameter list to fp */
+  fprintf(fp, "Simulation number: %d\n", SIM_NUM);
+  fprintf(fp, "Number of layers: %d\n", NUM_L);
+  fprintf(fp, "Number of rows: %d\n", NUM_R);
+  fprintf(fp, "Number of columns: %d\n", NUM_C);
+  fprintf(fp, "Initial temperature: %f\n", INIT_T);
+  fprintf(fp, "Final temperature: %f\n", FINAL_T);
+  fprintf(fp, "Temperature annealing interval dT: %f\n", DELTA_T);
+  fprintf(fp, "Initial external field strength B = %f\n", B_CONST);
+  fprintf(fp, "Field scanning interval dB = %f\n", DELTA_B);
+  fprintf(fp, "Anneal time: %d\n", ANNEAL_TIME);
+  fprintf(fp, "Equilibration time: %d\n", EQ_TIME);
+  fprintf(fp, "Correlation time: %d\n", COR_TIME);
+
+  fprintf(fp, "J_INTRA_CONST: [");
+  for(i = 0; i < NUM_L; i++){
+    fprintf(fp, " %f ", J_INTRA_CONST[i]);
+  }
+  fprintf(fp, "]\n");
+
+  fprintf(fp, "J_INTER_CONST: [");
+  for(i = 0; i < NUM_L; i++){
+    fprintf(fp, " %f ", J_INTER_CONST[i]);
+  }
+  fprintf(fp, "]\n");
+
+  fprintf(fp, "D_INTRA_CONST: [");
+  for(i = 0; i < NUM_L; i++){
+    fprintf(fp, " %f ", D_INTRA_CONST[i]);
+  }
+  fprintf(fp, "]\n");
+
+  fprintf(fp, "D_INTER_CONST: [");
+  for(i = 0; i < NUM_L; i++){
+    fprintf(fp, " %f ", D_INTER_CONST[i]);
+  }
+  fprintf(fp, "]\n");
+
+  fprintf(fp, "K_CONST: [");
+  for(i = 0; i < NUM_L; i++){
+    fprintf(fp, " %f ", K_CONST[i]);
+  }
+  fprintf(fp, "]\n");
+
+  fprintf(fp, "J_INTRA_P: %d\n", J_INTRA_P);
+  fprintf(fp, "J_INTER_P: %d\n", J_INTER_P);
+  fprintf(fp, "D_INTRA_P: %d\n", D_INTRA_P);
+  fprintf(fp, "D_INTER_P: %d\n", D_INTER_P);
+  fprintf(fp, "K_P: %d\n", K_P);
+  fprintf(fp, "RADIUS: %f\n", RADIUS);
+
+}
+
+void build_lattice(){
+  /* Allocate memory for the lattice structure */
+  /* Set the parameter interaction strengths
+
+  To do: define path for custom periodic functions
+  defining interaction strengths, initialize D vector
+  */
+  /* Generate random spins to populate the lattice */
+
+  int i, j, k;
+  lattice_t lattice = (spin_t ***)malloc(NUM_L*sizeof(spin_t **));
+  for(i = 0; i < NUM_L; i++){
+    lattice[i] = (spin_t **)malloc(NUM_R*sizeof(spin_t *));
+    for(j = 0; j < NUM_R; j++)
+      lattice[i][j] = (spin_t *)malloc(NUM_C*sizeof(spin_t));
+  }
+
+
+  if(!J_INTRA_P){
+    for(i = 0; i < NUM_L; i++)
+      for(j = 0; j < NUM_L; j++)
+        for(k = 0; k < NUM_L; k++)
+          J_INTRA[i][j][k] = J_INTRA_CONST[i];
+  }
+
+  if(!J_INTER_P){
+    for(i = 0; i < NUM_L; i++)
+      for(j = 0; j < NUM_L; j++)
+        for(k = 0; k < NUM_L; k++)
+          J_INTER[i][j][k] = J_INTER_CONST[i];
+  }
+
+  if(!D_INTRA_P){
+    for(i = 0; i < NUM_L; i++)
+      for(j = 0; j < NUM_L; j++)
+        for(k = 0; k < NUM_L; k++)
+          D_INTRA[i][j][k] = D_INTRA_CONST[i];
+  }
+
+  if(!D_INTER_P){
+    for(i = 0; i < NUM_L; i++)
+      for(j = 0; j < NUM_L; j++)
+        for(k = 0; k < NUM_L; k++)
+          D_INTER[i][j][k] = D_INTER_CONST[i];
+  }
+
+  if(!K_P){
+    for(i = 0; i < NUM_L; i++)
+      for(j = 0; j < NUM_L; j++)
+        for(k = 0; k < NUM_L; k++)
+          K[i][j][k] = K_CONST[i];
+  }
+
+  rng = gsl_rng_alloc(gsl_rng_mt19937);
+  gsl_rng_set (rng, time(NULL));
+  for(i = 0; i < NUM_L; i++){
+    for(j = 0; j < NUM_R; j++){
+      for(k = 0; k < NUM_C; k++){
+        gen_random_spin(&lattice[i][j][k]);
+      }
+    }
+}
+
+}
+
+void gen_random_spin(spin_t* spin){
+    double x1, x2, mag_sq;
+    gsl_rng_uniform(rng);
+
+    x1 = gsl_rng_uniform(rng)*2.0 - 1.0;
+    x2 = gsl_rng_uniform(rng)*2.0 - 1.0;
+    mag_sq = gsl_pow_2(x1) + gsl_pow_2(x2);
+
+    while(mag_sq >= 1){
+      x1 = gsl_rng_uniform(rng)*2.0 - 1.0;
+      x2 = gsl_rng_uniform(rng)*2.0 - 1.0;
+      mag_sq = gsl_pow_2(x1) + gsl_pow_2(x2);
+    }
+
+    spin->x = 2.0*x1*sqrt(1.0-mag_sq);
+    spin->y = 2.0*x2*sqrt(1.0-mag_sq);
+    spin->z = 1.0-2.0*mag_sq;
+
+}
---
+++ plot_h3d_nlayer.py
@@ -0,0 +1,336 @@
+from __future__ import print_function
+import ctypes
+import pickle
+from functools import reduce
+from scipy.integrate import simps
+import shelve
+from random import choice
+from numpy import exp, power
+from numpy.random import randint, rand, seed
+import logging
+from matplotlib import pyplot, colors
+from scipy.optimize import curve_fit
+import time
+
+SIM_NUM = 1
+NUM_L = 1
+ROWS = 20
+COLS = 20
+
+PDOUBLE = ctypes.POINTER(ctypes.c_double)
+PPDOUBLE = ctypes.POINTER(PDOUBLE)
+
+_h3d = ctypes.CDLL('./heisenberg3d_nlayer.so')
+
+class Spin_t(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_double),
+                ("y", ctypes.c_double),
+                ("z", ctypes.c_double)]
+
+PSPIN_T = ctypes.POINTER(Spin_t)
+PPSPIN_T = ctypes.POINTER(PSPIN_T)
+PPPSPIN_T = ctypes.POINTER(PPSPIN_T)
+
+
+
+_h3d.initialize_lattice.argtypes = ()
+_h3d.M_v_B.argtypes = (ctypes.POINTER(PDOUBLE),)
+_h3d.M_v_B.restype = ctypes.c_int
+_h3d.record_lattice.argtypes = (ctypes.POINTER(PPSPIN_T),)
+_h3d.record_lattice.restype = None
+_h3d.sample_mag.argtypes = (PDOUBLE, ctypes.c_int)
+_h3d.sample_mag.restype = None
+
+_h3d.mag_v_temp.argtypes = (ctypes.POINTER(PDOUBLE),)
+_h3d.mag_v_temp.restype = ctypes.c_int
+_h3d.suscept_v_temp.argtypes = (ctypes.POINTER(PDOUBLE),)
+_h3d.suscept_v_temp.restype = ctypes.c_int
+
+
+#_h3d.staggered_mag_v_temp.argtypes = (ctypes.POINTER(PDOUBLE),)
+#_h3d.staggered_mag_v_temp.restype = ctypes.c_int
+#_h3d.staggered_suscept_v_temp.argtypes = (ctypes.POINTER(PDOUBLE),)
+#_h3d.staggered_suscept_v_temp.restype = ctypes.c_int
+#_h3d.M_v_T.argtypes = (ctypes.POINTER(PDOUBLE), ctypes.c_double, ctypes.c_double, ctypes.c_double)
+#_h3d.M_v_T.restype = ctypes.c_int
+#_h3d.M_v_K.argtypes = (ctypes.POINTER(PDOUBLE),)
+#_h3d.M_v_K.restype = ctypes.c_int
+#_h3d.M_v_J.argtypes = (ctypes.POINTER(PDOUBLE),)
+#_h3d.M_v_J.restype = ctypes.c_int
+
+def autocorrelate_mag(sweeps, selection):
+    # Calc and plot magnetization autocorrelation function as a function of sweeps
+    # Average magnetization per site
+
+    # self.mag_vals should hold all the magnetization per spin values calculated at the end of
+    # each sweep.
+    global _h3d
+    mag_vals_arr = ctypes.c_double*sweeps
+    mag_vals = mag_vals_arr()
+
+    _h3d.initialize_lattice()
+    _h3d.sample_mag(mag_vals, sweeps)
+
+    avg_mag = reduce((lambda x,y: x+y), mag_vals)/len(mag_vals)
+    avg_mag_sq = power(avg_mag, 2)
+
+    chi_0 = autocorrelate_int(avg_mag_sq, 0, sweeps, mag_vals) # Initial autocorrelation function chi(0)
+    x = [i for i in range(selection)]
+    y = [autocorrelate_int(avg_mag_sq, i, sweeps, mag_vals, norm=chi_0) for i in range(selection)]
+    pyplot.plot(x, y, 'b')
+    pyplot.ylabel('Magnetization autocorrelation $\chi(t)$')
+    pyplot.xlabel('Sweeps')
+
+    pyplot.show()
+
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump([x,y], fp)
+
+
+def autocorrelate_int(avg_mag_sq, t, sweep_num, mag_vals, norm = 1):
+    # Performs autocorrelation integral at sweep t
+    # Can only integrate over t'=0 to t'=(self.step_num/self.lat_size)-t ?
+    #t_prime = [i for i in range((int(self.step_num/self.lat_size) - t))] # x samples
+    print("Autocorrelating with t = ", t)
+    t_prime = [i for i in range(sweep_num - t)] # x samples
+    y_samples = [((mag_vals[i]*mag_vals[i+t]) - avg_mag_sq)/norm for i in t_prime]
+    return simps(y_samples, t_prime)
+
+'''
+def plot_staggered_mag():
+    global _h3d
+
+    mag_vals_entry = ctypes.c_double*2
+    mag_vals_arr = PDOUBLE*10000
+
+    mag_vals = mag_vals_arr()
+
+    for i in range(10000):
+        mag_vals[i] = mag_vals_entry()
+
+    _h3d.initialize_lattice()
+    num_samples = _h3d.staggered_mag_v_temp(mag_vals)
+
+    data = [[] for i in range(2)]
+    for i in range(num_samples):
+        data[0].append(mag_vals[i][0])
+        data[1].append(mag_vals[i][1])
+
+
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump(data, fp)
+'''
+def plot_mag():
+    global _h3d
+
+    mag_vals_entry = ctypes.c_double*2
+    mag_vals_arr = PDOUBLE*10000
+
+    mag_vals = mag_vals_arr()
+
+    for i in range(10000):
+        mag_vals[i] = mag_vals_entry()
+
+    _h3d.initialize_lattice()
+    num_samples = _h3d.mag_v_temp(mag_vals)
+
+    data = [[] for i in range(2)]
+    for i in range(num_samples):
+        data[0].append(mag_vals[i][0])
+        data[1].append(mag_vals[i][1])
+
+
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump(data, fp)
+
+'''
+def plot_staggered_suscept():
+    global _h3d
+
+    s_vals_entry = ctypes.c_double*2
+    s_vals_arr = PDOUBLE*10000
+
+    s_vals = s_vals_arr()
+
+    for i in range(10000):
+        s_vals[i] = s_vals_entry()
+
+    _h3d.initialize_lattice()
+    num_samples = _h3d.staggered_suscept_v_temp(s_vals)
+
+    data = [[] for i in range(2)]
+    for i in range(num_samples):
+        data[0].append(s_vals[i][0])
+        data[1].append(s_vals[i][1])
+
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump(data, fp)
+'''
+def plot_suscept():
+    global _h3d
+
+    s_vals_entry = ctypes.c_double*2
+    s_vals_arr = PDOUBLE*10000
+
+    s_vals = s_vals_arr()
+
+    for i in range(10000):
+        s_vals[i] = s_vals_entry()
+
+    _h3d.initialize_lattice()
+    num_samples = _h3d.suscept_v_temp(s_vals)
+
+    data = [[] for i in range(2)]
+    for i in range(num_samples):
+        data[0].append(s_vals[i][0])
+        data[1].append(s_vals[i][1])
+
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump(data, fp)
+
+
+
+def plot_lattice():
+    global _h3d
+
+    col_arr = Spin_t*COLS
+    row_arr = PSPIN_T*ROWS
+    lattice_arr = PPSPIN_T*NUM_L
+
+    record = lattice_arr()
+    for i in range(NUM_L):
+        record[i] = row_arr()
+        for j in range(ROWS):
+            record[i][j] = col_arr()
+
+    _h3d.initialize_lattice()
+    _h3d.record_lattice(record)
+
+    data = [[] for i in range(NUM_L)]
+    for i in range(NUM_L):
+        for j in range(ROWS):
+            data[i].append([])
+            for k in range(COLS):
+                data[i][j].append((record[i][j][k].x, record[i][j][k].y, record[i][j][k].z))
+
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump(data, fp)
+
+
+def plot_M_v_B(max_samples = 10000):
+    """ Plots the mean magnetization per spin vs. external field strength.
+        Data is acquired by calling the M_v_B() function in the C implementation.
+
+        Plots will include separate sub-figures for total magnetization M, and
+        individual layer magnetizations (identified as M1, M2, M3, M4).
+
+        The number of data points returned by the C function is available as
+        num_samples. The data will populate the results list, where results[i][0]
+        is the external field strength of the data point and results[i][1] is the
+        total magnetization. The data results[i][2:5] represent the magnetization
+        values of the individual layers.
+    """
+    global _h3d
+
+    # Create results list to pass by reference
+    sample_arr = ctypes.c_double*(NUM_L + 3) # B, M, M1, M2, M3, M4, ... array for specific B value
+    data_arr = PDOUBLE*max_samples
+
+    results = data_arr()
+    for i in range(max_samples):
+        results[i] = sample_arr()
+
+    _h3d.initialize_lattice()
+    num_samples = _h3d.M_v_B(results) # pass results list by ref to be populated by C function
+    ''''
+    B_vals = []
+    M_vals = []
+    M1_vals = []
+    M2_vals = []
+    M3_vals = []
+    M4_vals = []
+    for i in range(num_samples):
+        B_vals.append(results[i][0])
+        M_vals.append(results[i][1])
+        M1_vals.append(results[i][2])
+        M2_vals.append(results[i][3])
+        M3_vals.append(results[i][4])
+        M4_vals.append(results[i][5])
+    '''
+    data = [[] for i in range((NUM_L + 3))]
+    for i in range(num_samples):
+        for j in range(NUM_L + 3):
+            data[j].append(results[i][j])
+    '''
+        B_vals.append(results[i][0])
+        M_vals.append(results[i][1])
+        M1_vals.append(results[i][2])
+        M2_vals.append(results[i][3])
+        M3_vals.append(results[i][4])
+        M4_vals.append(results[i][5])
+    '''
+    '''
+    plt.subplot(511)   # Magnetization per spin, all lattices
+    plt.plot(B_vals[0:int(num_samples/2)], M_vals[0:int(num_samples/2)], 'r')
+    plt.plot(B_vals[int(num_samples/2):], M_vals[int(num_samples/2):], 'b')
+    plt.ylim(-1.0, 1.0)
+
+    plt.ylabel("M")
+    #plt.xlabel("B")
+    plt.xticks([])
+
+    plt.subplot(512)   # Magnetization per spin, first lattice
+    plt.plot(B_vals[0:int(num_samples/2)], M1_vals[0:int(num_samples/2)], 'r')
+    plt.plot(B_vals[int(num_samples/2):], M1_vals[int(num_samples/2):], 'b')
+    plt.ylim(-1.0, 1.0)
+
+
+    plt.ylabel("$M_{1}$")
+    plt.xticks([])
+    #plt.xlabel("B")
+
+    plt.subplot(513)   # Magnetization per spin, second lattice
+    plt.plot(B_vals[0:int(num_samples/2)], M2_vals[0:int(num_samples/2)], 'r')
+    plt.plot(B_vals[int(num_samples/2):], M2_vals[int(num_samples/2):], 'b')
+    plt.ylim(-1.0, 1.0)
+
+
+    plt.ylabel("$M_{2}$")
+    plt.xticks([])
+
+    #plt.xlabel("B")
+
+    plt.subplot(514)   # Magnetization per spin, third lattice
+    plt.plot(B_vals[0:int(num_samples/2)], M3_vals[0:int(num_samples/2)], 'r')
+    plt.plot(B_vals[int(num_samples/2):], M3_vals[int(num_samples/2):], 'b')
+    plt.ylim(-1.0, 1.0)
+
+
+    plt.ylabel("$M_{3}$")
+    #plt.xlabel("B")
+    plt.xticks([])
+
+
+    plt.subplot(515)   # Magnetization per spin, fourth lattice
+    plt.plot(B_vals[0:int(num_samples/2)], M4_vals[0:int(num_samples/2)], 'r')
+    plt.plot(B_vals[int(num_samples/2):], M4_vals[int(num_samples/2):], 'b')
+    plt.ylim(-1.0, 1.0)
+
+
+    plt.ylabel("$M_{4}$")
+    plt.xlabel("B (J_inter = {.05,.05,.05,0}, K = {.05,.05,.05,.05}, J_intra = {1.0,1.0,1.0,1.0})")
+    #plt.xlabel("$H$ ($J_{FM} = 1.0$, $J_{AFM} = .05$, $K_{1} = .06$, $K_{234} = .05$)")
+    plt.savefig("sim_results/sim_"+str(SIM_NUM)+".png")
+    '''
+    with open("sim_results/sim_"+str(SIM_NUM)+".pickle", 'wb') as fp:
+        pickle.dump(data, fp)
+
+    #plt.show()
+
+if __name__ == "__main__":
+    #plot_lattice()
+    #autocorrelate_mag(300000, 3000)
+    #plot_mag()
+    plot_M_v_B()
+    #plot_suscept()
+    #plot_staggered_mag()
